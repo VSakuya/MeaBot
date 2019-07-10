bb7c536559306f8b4643c4125b6baccefafe4a22
VSakuya <vsakuya@outlook.com>
2019-07-10 23:03:56 +0800

    create data file automatically

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 from os import path
+import os
 import nonebot
 import config
 
 if __name__ == '__main__':
+    if not path.exists('.' + os.sep + 'asset' + os.sep + 'data' + os.sep):
+        os.mkdir('.' + os.sep + 'asset' + os.sep + 'data' + os.sep)
     nonebot.init(config)
     nonebot.load_plugins(
         path.join(path.dirname(__file__), 'asset', 'plugins'),
