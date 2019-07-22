dc523420643d951445e8b1fe9b454c90069fa223
VSakuya <vsakuya@outlook.com>
2019-07-23 01:38:36 +0800

    set log time as UTC+8

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import config
 import logging
 import math
 import time
+from pytz import timezone, utc
+from datetime import datetime
 from nonebot.log import logger
 
 def check_file():
@@ -13,6 +15,12 @@ def check_file():
         if not os.path.exists(n_dir):
             os.mkdir(n_dir)
     logger.info('Dirs checked')
+
+def customTime(*args):
+    utc_dt = utc.localize(datetime.utcnow())
+    my_tz = timezone("Asia/Shanghai")
+    converted = utc_dt.astimezone(my_tz)
+    return converted.timetuple()
 
 if __name__ == '__main__':
     if not path.exists(path.join(path.dirname(__file__), 'asset', 'data')):
@@ -26,6 +34,7 @@ if __name__ == '__main__':
         filename = log_dir, 
         format = '%(asctime)s-%(name)s-%(funcName)s-%(levelname)s-%(message)s'
     )
+    logging.Formatter.converter = customTime
     nonebot.load_plugins(
         path.join(path.dirname(__file__), 'asset', 'plugins'),
         'asset.plugins'
