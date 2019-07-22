99b9c4ec288e06ddda3a1561512ce5edb1ebd88f
VSakuya <vsakuya@outlook.com>
2019-07-23 02:32:02 +0800

    bug fixed

--- asset/plugins/autoreply/data_source.py
+++ asset/plugins/autoreply/data_source.py
@@ -153,28 +153,29 @@ async def add_reply_data(group_id, key : str, message : list, submit_user_id : i
     with open(os.getcwd() + os.sep + 'asset' + os.sep + 'data' + os.sep +'autoreply.json' , 'r', encoding='utf-8') as data_json:
         data_dict = json.load(data_json)
         key_list = key.split('|')
-        if not group_id:
-            if not '0' in data_dict:
-                data_dict['0'] = {}
-            item = data_dict['0']
-            new_content = {
-            'message': message,
-            'sumbit_user_id' : submit_user_id,
-            'checked_user_id' : checked_user_id
-            }
-            item[str(key)] = new_content
-            data_dict['0'] = item
-        else:
-            if not str(group_id) in data_dict:
-                data_dict[str(group_id)] = {}
-            item = data_dict[str(group_id)]
-            new_content = {
-            'message': message,
-            'sumbit_user_id' : submit_user_id,
-            'checked_user_id' : checked_user_id
-            }
-            item[str(key)] = new_content
-            data_dict[str(group_id)] = item
+        for single_key in key_list:
+            if not group_id:
+                if not '0' in data_dict:
+                    data_dict['0'] = {}
+                item = data_dict['0']
+                new_content = {
+                'message': message,
+                'sumbit_user_id' : submit_user_id,
+                'checked_user_id' : checked_user_id
+                }
+                item[str(single_key)] = new_content
+                data_dict['0'] = item
+            else:
+                if not str(group_id) in data_dict:
+                    data_dict[str(group_id)] = {}
+                item = data_dict[str(group_id)]
+                new_content = {
+                'message': message,
+                'sumbit_user_id' : submit_user_id,
+                'checked_user_id' : checked_user_id
+                }
+                item[str(single_key)] = new_content
+                data_dict[str(group_id)] = item
 
     if not data_dict:
         return False
